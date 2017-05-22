e5de8974de79e4702b420591eafd4d0c410ac864
Serge Menshikh <sergemenshikh@gmail.com>
2017-05-22 14:08:02 +0100

    10.05.2017

---
+++ JavaLearningAddNumbers.java
@@ -0,0 +1,27 @@
+package com.company;
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class JavaLearningAddNumbers {
+
+    public int sumOfNumbers( int number) throws Exception {
+
+        if (number <= 1){
+            throw new Exception("Number should be bigger then 0.");
+        }
+
+        int sum=0;
+
+        for(int i =1; i<= number;i++){
+
+            sum = sum+i;
+        }
+
+        return sum;
+
+    }
+
+
+}
